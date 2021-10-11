cb14bc67505e69c8a3541d6a7fb3265f2bdd8cad
Tpereletegui <tpereletegui@hotmail.com>
2021-10-11 16:03:54 -0300

    deploy in progress

--- index.js
+++ index.js
@@ -18,16 +18,25 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
+const express= require("express");
 const { conn, Country} = require('./src/db.js');
 const axios=require("axios");
 const {data} =require("./src/controllers/countries");
+const cors= require("cors");
+const path=require("path")
+const PORT = process.env.PORT || 3001
+server.use(cors());
 
+if(process.env.NODE_ENV === "production") {
+	server.use(express.static(path.join(__dirname,"client/build")))
+}
 
+console.log(__dirname,"client/build")
 
 // Syncing all the models at once.
-conn.sync({force: false}).then(async function() {
-  server.listen(3001, async () => {
-    console.log('%s listening at 3001');
+conn.sync({force: true}).then(async function() {
+  server.listen(PORT, async () => {
+    console.log("listening at port " + PORT);
     let checkdatabase= await Country.findAll();
      if(checkdatabase.length <=0 )  data();
     // eslint-disable-line no-console
--- src/app.js
+++ src/app.js
@@ -3,13 +3,24 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const cors= require("cors");
+const path=require("path")
+const PORT = process.env.PORT || 3001
+
+
 
 require('./db.js');
 
 const server = express();
 
-server.name = 'API';
+server.use(cors());
 
+if(process.env.NODE_ENV === "production") {
+	server.use(express.static(path.join(__dirname,"client/build")))
+}
+
+server.name = 'API';
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
